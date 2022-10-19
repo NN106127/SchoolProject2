1c78c8f2ba119262ba97d38c3157dd5344da3e9d
NN106127 <ppnsean@gmail.com>
2022-10-20 00:33:07 +0800

    Enemy 狀態機結構製作

--- Assets/Scenes/Script/Enmey.cs
+++ Assets/Scenes/Script/Enmey.cs
@@ -6,12 +6,14 @@ public class Enmey : MonoBehaviour
 {
     public GameObject EnmeyBlood;
     public GameObject TargetPlayer;
-    public float Speed;      //走路速度
-    public float TurnSpeed;  //轉向速度
-    public float TraceRadius;//追逐距離
-    float gravity = 20.0f;
+    public float Speed;      // 走路速度
+    public float TurnSpeed;  // 轉向速度
+    public float TraceRadius;// 追逐距離
+    //float gravity = 20.0f;
     Vector3 moveDirection = Vector3.zero, vDir = Vector3.zero;
     private CharacterController controller;
+
+    public string status = "patrol";   // patrol = 巡邏; chasing = 追逐; dead = 死亡;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +23,60 @@ public class Enmey : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y >= -0.85)
+        // 巡邏狀態
+        if (status == "patrol")
+        {
+            // 巡邏行為
+            DoPatrol();
+
+            // 改狀態為追逐的判斷
+            float dist = Vector3.Distance(transform.position, TargetPlayer.transform.position);
+            if (dist < TraceRadius)
+            {
+                status = "chasing";
+            }
+
+            // 改狀態為死亡的判斷
+            if (EnmeyBlood.transform.position.x >= -1)
+            {
+                status = "dead";
+            }
+        }
+
+        if (status == "chasing")
+        {
+            DoChasing();
+        }
+
+        if (status == "dead")
+        {
+            DoDead();
+        }
+        /*if (transform.position.y >= -0.85)
         {
             vDir.y -= gravity * Time.deltaTime;
         }
 
         EnmeyMove();
 
-        controller.Move((moveDirection + vDir) * Time.deltaTime);
+        controller.Move((moveDirection + vDir) * Time.deltaTime);*/
     }
 
-    private void OnDrawGizmos()
+    void DoPatrol()
+    {
+
+    }
+
+    void DoChasing()
+    {
+
+    }
+
+    void DoDead()
+    {
+
+    }
+    /*private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, TraceRadius);
@@ -50,7 +95,7 @@ public class Enmey : MonoBehaviour
             Vector3 dir = (TargetPlayer.transform.position - transform.position).normalized;
             controller.Move(dir * Speed * Time.deltaTime);
         }
-    }
+    }*/
 
     private void OnTriggerEnter(Collider other)
     {
@@ -59,7 +104,7 @@ public class Enmey : MonoBehaviour
             EnmeyBlood.transform.Translate(-0.5f, 0, 0);
             if (EnmeyBlood.transform.position.x >= -1)
             {
-                Destroy(gameObject);
+                Destroy(gameObject, 2);
             }
         }
     }
