be2b8df13596a0420c6feeb8b4a2731c26319202
NN106127 <ppnsean@gmail.com>
2022-11-21 23:56:14 +0800

    boss 程式更新

--- Assets/Script/Boss.cs
+++ Assets/Script/Boss.cs
@@ -4,58 +4,95 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    public GameObject TargetPlayer;
-    public float distance;
-    public float TraceRadius;  //追逐距離
-    public float TurnSpeed;
-    public string status  = "patrol";
-    public float BossSkillTimer = 0;
-
-    public int i = 0;
-
-    public GameObject m_rockPrefab;
-    public Transform m_rightHandTransform;
+    public GameObject TargetPlayer;// 玩家
+    public GameObject bullet;
+    public GameObject firePoint;
+    public GameObject Effect;  // 毒氣特效
+    public int i;              // 隨機紀能編號
+    public int n = 0;          // 控制子彈只射一顆
+    private CharacterController controller;
+    public string status = "patrol";
+    public string prevStatus = "patrol";
+    public float ColdTimer = 0;//技能之間冷卻
 
     // Start is called before the first frame update
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
+        Effect.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(transform.position, TargetPlayer.transform.position);
-        if (distance < 15)
+        if (status == "Random")
         {
-            //LookPlayer();
-            status = "ATK";
+            n = 0;
+            Effect.SetActive(false);
+            ColdTimer = 0;
+            RandomSkill();
+            prevStatus = status;
+            status = "Atk";
+        }
+
+        if (status == "Cold")
+        {
+            DoColdTime();
+            if (ColdTimer >= 3)
+            {
+                Debug.Log("Random");
+                prevStatus = status;
+                status = "Random";
+            }
+        }
+
+        if (status == "Atk")
+        {
+            if (i == 1)
+            {
+                if (n == 0)
+                {
+                    DoSkill01();
+                    n = 1;
+                    prevStatus = status;
+                    status = "Cold";
+                }
+            }
+
+            if (i == 2)
+            {
+                DoSkill02();
+                prevStatus = status;
+                status = "Cold";
+            }
         }
     }
 
-    private void OnDrawGizmos()
+    void DoColdTime() // 冷卻時間
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, TraceRadius);
+        ColdTimer += Time.deltaTime;
     }
 
-
-    void LookPlayer()
+    void RandomSkill() // 隨機抽技能
     {
-        float dist = Vector3.Distance(transform.position, TargetPlayer.transform.position);
-        if (dist < TraceRadius)
+        i = Random.Range(1, 3);
+    }
+
+    void DoSkill01()  // 技能一
+    {
+        Instantiate(bullet, firePoint.transform.position, transform.rotation);
+    }
+
+    void DoSkill02()  // 技能二
+    {
+        Effect.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
         {
-            //漸漸看向玩家
-            Vector3 relativePos = TargetPlayer.transform.position - transform.position;
-            Quaternion targetRotation = Quaternion.LookRotation(relativePos);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
-            Vector3 dir = (TargetPlayer.transform.position - transform.position).normalized;
+            status = "Random";
         }
     }
-
-    void RockThrow()
-    {
-        GameObject rock = Instantiate(m_rockPrefab, m_rightHandTransform.position, Quaternion.identity);
-    }
-
 }
