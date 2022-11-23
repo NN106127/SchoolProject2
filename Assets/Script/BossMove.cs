1857f207aceb696c66e3eae0977273b8f196bae2
specteruwu <98225008+specteruwu@users.noreply.github.com>
2022-11-23 23:50:27 +0800

    一些更改

--- Assets/Script/BossMove.cs
+++ Assets/Script/BossMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BossMove : MonoBehaviour
 {
+    public float TurnSpeed;
+    public float radius;
+    public float stopradius;
     public GameObject TargetPlayer;// ª±®a
     public float TraceRadius;// °l³v¶ZÂ÷
     public float Speed;
@@ -19,15 +22,33 @@ public class BossMove : MonoBehaviour
     {
         // °l³v
         float dist = Vector3.Distance(transform.position, TargetPlayer.transform.position);
-        if (dist <= 30)
+        if (dist < TraceRadius)
         {
+            Vector3 relativePos = TargetPlayer.transform.position - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
+            Speed = 10f;
             Vector3 dir = (TargetPlayer.transform.position - transform.position).normalized;
             controller.Move(dir * Speed * Time.deltaTime);
+            if (dist < radius)
+            {
+                Speed = 0;
+                controller.Move(dir * Speed * Time.deltaTime);
+            }
+            if (dist < stopradius)
+            {
+                Speed = 0;
+                controller.Move(dir * Speed * Time.deltaTime);
+            }
         }
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, TraceRadius);
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, stopradius);
     }
 }
